909ad8016c4a97af5705ef11074b734913aba073
sthumkunta-rythmos <31001165+sthumkunta-rythmos@users.noreply.github.com>
2018-01-17 11:26:52 +0530

    Testing

---
+++ src/Employee.java
@@ -0,0 +1,53 @@
+
+/*public class Employee {
+	
+	int empid;
+	String empname;
+	
+Employee(int id, String name){
+	this.empid = id;
+	this.empname = name;
+	
+}
+
+void main() {
+	
+	System.out.println("id :"+empid+ " name :"+empname);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Employee e1= new Employee(2342,"twewre");
+		Employee e2 = new Employee(3243,"dfdfd");
+				
+		e1.main();
+		e2.main();
+		
+	}
+
+}*/
+
+
+public class Employee{
+	int empid;
+	String empname;
+
+	Employee(int id, String name){
+	this.empid = id;
+	this.empname = name;
+	
+	}
+	void main(){
+	System.out.println("id: "+empid+"name: "+empname);
+	}
+	
+	public static void main(String args[]){
+	
+	Employee e1 = new Employee(161,"eqfwe");
+	Employee e2 = new Employee(112,"rreqr");
+	e1.main();
+	e2.main();
+	
+	}
+	
+	}
